b949a638e5b1c4a61b235ba31e59383a5c68cdeb
Chris Hager <chris.hager@bitsworking.com>
2014-08-25 19:38:50 +0200

    web2serial websocket handler

--- README.md
+++ README.md
@@ -12,4 +12,47 @@ metachris, overflo
 
 License
 =======
+
 LGPLv3 (see `LICENSE`).
+
+
+web2serial library
+==================
+
+Dependencies: [tornado](https://github.com/tornadoweb/tornado)
+
+Port Number: 54321
+
+REST Interface
+
+    GET /ping         Heartbeat, Check if exists
+    GET /devices      Returns list of devices
+
+    SCK /device/<device-id>/open/<baudrate>    Websocket Address for opening a device by id (with baudrate)
+
+
+jQuery GET example (http://api.jquery.com/jquery.get/):
+
+    var jqxhr = $.get("/ping", function() {
+        alert( "success" );
+    })
+    .done(function() {
+        alert( "second success" );
+    })
+    .fail(function() {
+        alert( "error" );
+    })
+    .always(function() {
+        alert( "finished" );
+    });
+
+
+Websocket JavaScript Example:
+
+    var ws = new WebSocket("ws://localhost:8888/websocket");
+    ws.onopen = function() {
+       ws.send("Hello, world");
+    };
+    ws.onmessage = function (evt) {
+       alert(evt.data);
+    };
--- src/web2serial.py
+++ src/web2serial.py
@@ -11,18 +11,100 @@ __author__ = "Chris Hager"
 __email__ = "chris@bitsworking.com"
 __version__ = "0.1"
 
+# CONFIG_FILE = "config.yaml"
+
 import sys
 import os
-import yaml
+# import yaml
 from optparse import OptionParser
 from pprint import pprint
 
-CONFIG_FILE = "config.yaml"
+import logging
+import os.path
+import uuid
+
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
+        ]
+
+        settings = dict(
+            cookie_secret="asdasdas87D*A8a7sd8T@*2",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html", messages=ChatSocketHandler.cache)
+
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def get_compression_options(self):
+        # Non-None enables compression with default options.
+        return {}
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
+
+    @classmethod
+    def send_updates(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            }
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat))
+
+        ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
 
 
 def main(options, args):
-    config = yaml.load(open(CONFIG_FILE))
-    pprint(config)
+    tornado.options.parse_command_line()
+    logging.info("Starting server on port %s" % options.port)
+
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == '__main__':
@@ -31,11 +113,15 @@ if __name__ == '__main__':
     Example: '%prog abc' or '%prog xyz'"""
     version = "%prog " + __version__
     parser = OptionParser(usage=usage, version=version)
+
     parser.add_option("-v", "--verbose", default=False,
         action="store_true", dest="verbose")
 
+    parser.add_option("-p", "--port", default=54321, \
+          dest="port")
+
     (options, args) = parser.parse_args()
-    if len(args) == 0:
-        parser.error("Please add at least one argument")
+    # if len(args) == 0:
+        # parser.error("Please add at least one argument")
 
     main(options, args)
