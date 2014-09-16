ad42dedfecff799c1660d8e5c5e70b3f058e85ae
Chris Hager <chris@metachris.org>
2014-09-16 21:05:18 +0200

    example with error and onclose handling

--- software/web2serial-javascript/web2serial.js
+++ software/web2serial-javascript/web2serial.js
@@ -10,6 +10,17 @@
  *     // Get a WebSocket connection to a serial device:
  *     ws_conn = web2serial.open_connection(device-hash, baudrate, onmessage_callback)
  *
+ * For the WebSocket API see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
+ *
+ * WebSocket has 2 methods:
+ * 
+ *     void send(in DOMString data);
+ *     void close(in optional unsigned long code, in optional DOMString reason);
+ *
+ * Sending data to the web2serial service requires a JSON object like this:
+ * { "msg": "your-bytes-for-the-serial-connection" }. You can use the helper 
+ * method `web2serial.jsonify(str)` to build the final JSON string.
+ *
  */
 var web2serial = {
     get_devices: function(callback) {
@@ -22,7 +33,7 @@ var web2serial = {
                     "hash":  _devices[i][0],
                     "device": _devices[i][1],
                     "desc": _devices[i][2],
-                    "hwinfo": _devices[i][3],                
+                    "hwinfo": _devices[i][3],
                 }
                 devices.push(device);
             }
@@ -37,5 +48,9 @@ var web2serial = {
         socket = new WebSocket(url);
         socket.onmessage = onmessage_callback;
         return socket;
+    },
+
+    jsonify: function(str) {
+        return JSON.stringify({ "msg": str });
     }
 }
