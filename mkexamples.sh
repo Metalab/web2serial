93879f686fc7463cd938c1f973ead60abcbcb583
Chris Hager <chris.hager@bitsworking.com>
2014-09-21 03:42:20 +0200

    cleanup bugfix

--- mkexamples.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp software/web2serial-javascript/web2serial.js examples/minimal/imports/web2serial.js
-cp software/web2serial-javascript/web2serial.js examples/magicshifter/imports/web2serial.js
